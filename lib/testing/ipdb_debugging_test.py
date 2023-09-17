717b3e353afc484e571f99da06b5b41d15065210
brianbryank <brianbryan0125@gmail.com>
2023-09-17 12:20:49 +0300

    passed

--- lib/testing/ipdb_debugging_test.py
+++ lib/testing/ipdb_debugging_test.py
@@ -8,3 +8,5 @@ class TestIpdbDebugging:
     def test_adds_two(self):
         '''adds_two() adds 2 to input arg and returns sum.'''
         assert(plus_two(3) == 5)
+def plus_two(x):
+    return x + 2
